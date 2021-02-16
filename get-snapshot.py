3bf359ec56a214d18ef7ff08ea768daed3e8b645
ankudinov <petr.ankudinov@gmail.com>
2021-02-16 18:57:36 +0100

    add timestamp

--- get-snapshot.py
+++ get-snapshot.py
@@ -10,8 +10,26 @@ import json
 import os
 import re
 from getpass import getpass
+from time import time as time
+from datetime import datetime as datetime
 
 nr = InitNornir(config_file='config.yaml')
+
+
+def time_stamp():
+    """
+    time_stamp function returns current system time as Y-M-D H-M-S string
+
+    Args:
+        no arguments required
+
+    Returns:
+        str: current system time as Y-M-D H-M-S string
+    """
+    time_not_formatted = time()
+    time_formatted = datetime.fromtimestamp(
+        time_not_formatted).strftime('%Y-%m-%d:%H:%M:%S.%f')
+    return time_formatted
 
 
 def get_config(task):
@@ -56,9 +74,9 @@ def run_a_command_list(task, a_command_list):
 if __name__ == "__main__":
     # create base directories if they do not exist
     if not os.path.isdir('configs'):
-            os.mkdir('configs')
+        os.mkdir('configs')
     if not os.path.isdir('show_commands'):
-            os.mkdir('show_commands')
+        os.mkdir('show_commands')
 
     # get username and password to avoid saving them
     username = input("Username: ")
@@ -73,9 +91,11 @@ if __name__ == "__main__":
 
     snapshot_command_list = list()
     with open('snapshot_commands.txt', 'r') as snapshot_commands_file:
-        snapshot_command_list = [ a_line.strip() for a_line in snapshot_commands_file ]
+        snapshot_command_list = [a_line.strip()
+                                 for a_line in snapshot_commands_file]
 
-    result = nr.run(task=run_a_command_list, a_command_list=snapshot_command_list)
+    result = nr.run(task=run_a_command_list,
+                    a_command_list=snapshot_command_list)
     if result.failed:
         print(result.failed_hosts)
         print_result(result)
